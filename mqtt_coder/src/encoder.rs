8cb397d31bd207e56c0a640d0c7be33de248b0c1
heya-naohiro <Naohiro.Heya@sony.com>
2025-02-27 22:39:36 +0900

    wip

--- mqtt_coder/src/encoder.rs
+++ mqtt_coder/src/encoder.rs
@@ -6,7 +6,6 @@ use crate::mqtt::{ControlPacket, MqttPacket};
 
 pub struct Encoder {
     state: EncodeState,
-    buffer: BytesMut,
 }
 
 enum EncodeState {
@@ -20,41 +19,39 @@ impl Encoder {
     pub fn new() -> Self {
         Encoder {
             state: EncodeState::FixedHeader,
-            buffer: BytesMut::new(),
         }
-    }
-    pub fn reset(&mut self) {
-        self.state = EncodeState::FixedHeader;
-        self.buffer.clear();
     }
 
     pub fn poll_encode(
         &mut self,
         cx: &mut Context<'_>,
         packet: &ControlPacket,
-    ) -> Poll<Option<Result<BytesMut, Box<dyn std::error::Error>>>> {
-        loop {
-            match self.state {
-                EncodeState::FixedHeader => {
-                    let fixed_header = packet.encode_fixed_header()?;
-                    self.buffer.extend_from_slice(&fixed_header);
-                    self.state = EncodeState::VariableHeader;
-                }
-                EncodeState::VariableHeader => {
-                    let variable_header = packet.encode_variable_header()?;
-                    self.buffer.extend_from_slice(&variable_header);
-                    self.state = EncodeState::Payload;
-                }
-                EncodeState::Payload => {
-                    if let Some(chunk) = packet.encode_payload_chunk()? {
-                        self.buffer.extend_from_slice(&chunk);
-                        return Poll::Ready(Some(Ok(self.buffer.split())));
-                    } else {
-                        self.state = EncodeState::Done;
-                    }
-                }
-                EncodeState::Done => return Poll::Ready(None),
+        buffer: &mut BytesMut,
+    ) -> Poll<Result<Option<()>, Box<dyn std::error::Error>>> {
+        match self.state {
+            EncodeState::FixedHeader => {
+                let fixed_header = packet.encode_fixed_header()?;
+                buffer.extend_from_slice(&fixed_header);
+                self.state = EncodeState::VariableHeader;
+                Poll::Ready(Ok(Some(())))
             }
+            EncodeState::VariableHeader => {
+                let variable_header = packet.encode_variable_header()?;
+                buffer.extend_from_slice(&variable_header);
+                self.state = EncodeState::Payload;
+
+                Poll::Ready(Ok(Some(())))
+            }
+            EncodeState::Payload => {
+                if let Some(chunk) = packet.encode_payload_chunk()? {
+                    buffer.extend_from_slice(&chunk);
+                    return Poll::Ready(Ok(Some(())));
+                } else {
+                    self.state = EncodeState::Done;
+                    return Poll::Ready(Ok(None));
+                }
+            }
+            EncodeState::Done => return Poll::Ready(Ok(None)),
         }
     }
 }
--- src/aqua/connection.rs
+++ src/aqua/connection.rs
@@ -1,29 +1,22 @@
 pub mod request;
 pub mod response;
 
-use bytes::{BufMut, BytesMut};
-use futures_util::stream::poll_fn;
+use bytes::Buf;
+use bytes::BytesMut;
 use mqtt_coder::decoder;
 use mqtt_coder::encoder;
 use mqtt_coder::mqtt;
-use mqtt_coder::mqtt::decoder::decode_fixed_header;
 use mqtt_coder::mqtt::ControlPacket;
 use mqtt_coder::mqtt::MqttError;
-use mqtt_coder::mqtt::MqttPacket;
 use request::Request;
 use response::Response;
 use std::future::Future;
-use std::mem;
-use std::net::SocketAddr;
 use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
-use tokio::io::AsyncWriteExt;
 use tokio::io::{AsyncRead, AsyncWrite};
-use tokio::net::{TcpListener, TcpStream};
 use tokio_util::io::poll_read_buf;
 use tower::Service;
-
 const BUFFER_CAPACITY: usize = 4096;
 
 pub struct Connection<S, IO>
@@ -36,6 +29,7 @@ where
     io: IO,
     state: ConnectionState<S::Future>,
     buffer: BytesMut,
+    write_buffer: BytesMut,
     decoder: decoder::Decoder,
     encoder: encoder::Encoder,
 }
@@ -63,6 +57,7 @@ where
             io,
             state: ConnectionState::PreConnection,
             buffer: BytesMut::with_capacity(BUFFER_CAPACITY), /* [TODO] limit */
+            write_buffer: BytesMut::new(),
             decoder: decoder::Decoder::new(),
             encoder: encoder::Encoder::new(),
         }
@@ -80,7 +75,7 @@ where
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let mut this = self.as_mut();
-        let mut new_state = None;
+        let new_state;
         match std::mem::take(&mut this.state) {
             ConnectionState::PreConnection => {
                 let _req = match this.as_mut().read_packet(cx) {
@@ -147,6 +142,7 @@ where
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
     ) -> Poll<Result<Request<mqtt::ControlPacket>, Box<dyn std::error::Error>>> {
+        // bufferにアクセスするためにPin<&mut Self> から &mut Self を取り出す
         let this = self.get_mut();
         let mut buf = &mut this.buffer;
         match poll_read_buf(Pin::new(&mut this.io), cx, &mut buf) {
@@ -162,34 +158,42 @@ where
             Poll::Ready(Err(e)) => Poll::Ready(Err(Box::new(e))),
         }
     }
+    // ここをloopなしにする
     fn write_packet(
         mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         res: &Response,
     ) -> Poll<Result<(), Box<dyn std::error::Error>>> {
+        // as_mut() は Pin<&mut Self> のまま再取得する。
         let mut this = self.as_mut();
-
-        this.encoder.reset();
-        loop {
-            match this.encoder.poll_encode(cx, &res.packet) {
-                Poll::Ready(Some(Ok(chunk))) => {
-                    match Pin::new(&mut this.io).poll_write(cx, &chunk) {
+        match this
+            .encoder
+            .poll_encode(cx, &res.packet, &mut this.write_buffer)
+        {
+            Poll::Ready(Ok(Some(()))) => {
+                while !this.buffer.is_empty() {
+                    match Pin::new(&mut this.io).poll_write(cx, &this.write_buffer) {
                         Poll::Ready(Ok(n)) => {
                             if n == 0 {
                                 return Poll::Ready(Err("Connection closed during write".into()));
                             }
+                            this.buffer.advance(n); // 書き込んだ分をバッファから削除
                         }
                         Poll::Ready(Err(e)) => return Poll::Ready(Err(Box::new(e))),
                         Poll::Pending => return Poll::Pending,
                     }
                 }
-                Poll::Ready(Some(Err(e))) => return Poll::Ready(Err(e)),
-                Poll::Ready(None) => {
-                    this.encoder.reset();
-                    return Poll::Ready(Ok(()));
-                }
-                Poll::Pending => return Poll::Pending,
+                //まだ続く、Encode起因のPending
+                cx.waker().wake_by_ref();
+                Poll::Pending
             }
+            Poll::Ready(Ok(None)) => {
+                // エンコードが完了
+                Poll::Ready(Ok(()))
+            }
+            Poll::Ready(Err(e)) => Poll::Ready(Err(e)), // エラー
+            Poll::Pending => Poll::Pending,             // エンコード待ち
         }
+        // [TODO] 必ずbufferは終わったらclearする
     }
 }
