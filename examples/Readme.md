059321a1f8b975ad8a98310fa174d41aa898ea5e
heya-naohiro <Naohiro.Heya@sony.com>
2025-04-23 21:15:11 +0900

    add interoperability test basin

---
+++ examples/Readme.md
@@ -0,0 +1,2 @@
+## Caution
+This broker.rs is a program for interoperability testing. Performance considerations are not taken into account.
--- examples/broker.rs
+++ examples/broker.rs
@@ -9,8 +9,8 @@ use tokio;
 use tokio::net::TcpListener;
 use tower::service_fn;
 
-#[tokio::test]
-async fn async_test() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let str_addr = "127.0.0.1:1883";
     let addr = str_addr.parse::<SocketAddr>().unwrap();
@@ -63,56 +63,11 @@ async fn async_test() -> Result<(), Box<dyn std::error::Error>> {
                 })
             }))
         });
-        /*
-        let make_service = service_fn(|incoming: request::IncomingStream| async move {
-            println!("New connection from: {:?}", incoming.addr);
-            Ok::<_, Infallible>(service_fn(|req: request::Request<ControlPacket>| {
-                Box::pin(async move {
-                    dbg!("Check request");
-                    match req.body {
-                        ControlPacket::CONNECT(connect_data) => {
-                            println!("Received CONNECT: {:?}", connect_data);
-                            let connack_packet = ControlPacket::CONNACK(Default::default());
-                            let response = response::Response::new(connack_packet);
-                            Ok::<_, std::io::Error>(response)
-                        }
-                        other => {
-                            println!("Received non-CONNECT packet: {:?}", other);
-                            Ok::<_, std::io::Error>(response::Response::default())
-                        }
-                    }
-                })
-            }))
-        });
-        */
         // `serve` を使ってサーバーを起動
         aqua::serve(listener, make_service, make_connect_service)
             .await
             .unwrap();
     });
 
-    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    {
-        let create_ops = paho_mqtt::CreateOptionsBuilder::new()
-            .server_uri("tcp://".to_owned() + str_addr)
-            .client_id("test-client-paho")
-            .finalize();
-        let client = paho_mqtt::AsyncClient::new(create_ops).unwrap();
-        let conn_opts = paho_mqtt::ConnectOptionsBuilder::new()
-            .keep_alive_interval(Duration::from_secs(5))
-            .clean_session(true)
-            .finalize();
-        println!("Connecting to MQTT broker...");
-
-        let ret = client.connect(conn_opts).await;
-        if let Err(e) = ret {
-            dbg!(e);
-        } else {
-            println!("Success!!");
-        }
-
-        tokio::time::sleep(Duration::from_secs(10)).await;
-        //client.disconnect().await.unwrap();
-    }
     Ok(())
 }
