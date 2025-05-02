9af2ca4e5b67c823961edbc597f2aba7f0b1732d
heya-naohiro <Naohiro.Heya@sony.com>
2025-05-02 11:03:00 +0900

    fix: error

--- examples/broker.rs
+++ examples/broker.rs
@@ -1,6 +1,7 @@
 use aqua::SESSION_MANAGER;
 use aqua::{request, response};
 use aqua::{ConnackError, ConnackResponse};
+use mini_redis::client;
 use mqtt_coder::mqtt::{
     self, Connack, ControlPacket, Pingresp, ProtocolVersion, Suback, SubackReasonCode,
     SubscribeOption,
@@ -8,98 +9,99 @@ use mqtt_coder::mqtt::{
 use paho_mqtt::topic;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use tokio;
 use tokio::net::TcpListener;
 use topic_manager::TopicManager;
 use tower::service_fn;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let str_addr = "127.0.0.1:1883";
     let addr = str_addr.parse::<SocketAddr>().unwrap();
-    let topic_mgr = TopicManager::new();
-    dbg!("Hello, async_test world");
-    tokio::spawn(async move {
-        let listener = TcpListener::bind(addr).await.unwrap();
-        let make_service = service_fn(|incoming: request::IncomingStream| async move {
-            println!("(normal) New connection from: {:?}", incoming.addr);
-            Ok::<_, Infallible>(service_fn(|req: request::Request<ControlPacket>| {
-                Box::pin(async move {
-                    /* client id error */
-                    let client_id = if let Some(client_id) = incoming.client_id {
-                        client_id
-                    } else {
-                        return Err(mqtt::MqttError::Unexpected);
-                    };
-
-                    println!("(normal) Received request");
-                    match req.body {
-                        ControlPacket::PINGREQ(_ping) => {
-                            return Ok(response::Response::new(ControlPacket::PINGRESP(
-                                Pingresp {},
-                            )))
-                        }
-                        ControlPacket::SUBSCRIBE(subpacket) => {
-                            let mut success_codes = vec![];
-                            for (filter, suboption) in subpacket.topic_filters {
-                                topic_mgr.register(filter.value(), client_id, suboption);
-                                success_codes.push(SubackReasonCode::from(suboption.qos));
+    let topic_mgr = Arc::new(TopicManager::new());
+    let make_service = {
+        let topic_mgr = Arc::clone(&topic_mgr);
+        service_fn(move |incoming: request::IncomingStream| {
+            let topic_mgr = Arc::clone(&topic_mgr);
+            let peer = incoming.addr;
+            let client_id_opt = incoming.client_id;
+            async move {
+                println!("(normal) New connection from: {:?}", peer);
+                Ok::<_, Infallible>(service_fn(move |req: request::Request<ControlPacket>| {
+                    let topic_mgr = Arc::clone(&topic_mgr);
+                    let client_id_opt = client_id_opt;
+                    Box::pin(async move {
+                        let client_id: Uuid = if let Some(client_id) = client_id_opt {
+                            client_id
+                        } else {
+                            return Err(mqtt::MqttError::Unexpected);
+                        };
+                        match req.body {
+                            ControlPacket::PINGREQ(_ping) => {
+                                return Ok(response::Response::new(ControlPacket::PINGRESP(
+                                    Pingresp {},
+                                )))
                             }
-                            return Ok(response::Response::new(ControlPacket::SUBACK({
-                                Suback {
-                                    packet_id: subpacket.packet_id,
-                                    suback_properties: None,
-                                    reason_codes: success_codes,
-                                    protocol_version: ProtocolVersion::new(0x05),
+                            ControlPacket::SUBSCRIBE(subpacket) => {
+                                let mut success_codes = vec![];
+                                for (filter, suboption) in subpacket.topic_filters {
+                                    topic_mgr.register(filter.value(), client_id, &suboption);
+                                    success_codes.push(SubackReasonCode::from(suboption.qos));
                                 }
-                            })));
-                        }
-                        ControlPacket::PUBLISH(_pubpacket) => {
-                            /* [TODO] topic implement -> clients */
-                            /* [TODO] another threads, because not need to sync */
+                                return Ok(response::Response::new(ControlPacket::SUBACK({
+                                    Suback {
+                                        packet_id: subpacket.packet_id,
+                                        suback_properties: None,
+                                        reason_codes: success_codes,
+                                        protocol_version: ProtocolVersion::new(0x05),
+                                    }
+                                })));
+                            }
+                            ControlPacket::PUBLISH(_pubpacket) => {
+                                /* [TODO] topic implement -> clients */
+                                /* [TODO] another threads, because not need to sync */
 
-                            // SESSION_MANAGER.send(client_id, pkt)
+                                // SESSION_MANAGER.send(client_id, pkt)
+                            }
+                            _ => {}
                         }
-                        _ => {}
+                        Ok(response::Response::default())
+                    })
+                }))
+            }
+        })
+    };
+    let make_connect_service = service_fn(|incoming: request::IncomingStream| async move {
+        println!("(connect) New connection from: {:?}", incoming.addr);
+        Ok::<_, Infallible>(service_fn(|req: request::Request<ControlPacket>| {
+            Box::pin(async move {
+                println!("(connect) Processing request");
+                match req.body {
+                    ControlPacket::CONNECT(_connect_data) => {
+                        // CONNECT パケットを受け取ったとき
+                        let connack_data = Connack {
+                            session_present: false,
+                            connect_reason: mqtt::ConnackReason::Success,
+                            connack_properties: None,
+                            version: ProtocolVersion::new(0x04),
+                        };
+                        let connack_response = ConnackResponse::from(connack_data);
+                        dbg!("(connect) Connack response, ", &connack_response);
+                        Ok(connack_response)
                     }
-                    Ok::<_, mqtt::MqttError>(response::Response::default())
-                })
-            }))
-        });
-        // CONNECT 用サービス
-        let make_connect_service = service_fn(|incoming: request::IncomingStream| async move {
-            println!("(connect) New connection from: {:?}", incoming.addr);
-            Ok::<_, Infallible>(service_fn(|req: request::Request<ControlPacket>| {
-                Box::pin(async move {
-                    println!("(connect) Processing request");
-                    match req.body {
-                        ControlPacket::CONNECT(_connect_data) => {
-                            // CONNECT パケットを受け取ったとき
-                            let connack_data = Connack {
-                                session_present: false,
-                                connect_reason: mqtt::ConnackReason::Success,
-                                connack_properties: None,
-                                version: ProtocolVersion::new(0x04),
-                            };
-                            let connack_response = ConnackResponse::from(connack_data);
-                            dbg!("(connect) Connack response, ", &connack_response);
-                            Ok(connack_response)
-                        }
-                        _ => {
-                            println!("(connect) Received non-CONNECT packet");
-                            Ok(ConnackResponse::default())
-                        }
+                    _ => {
+                        println!("(connect) Received non-CONNECT packet");
+                        Ok(ConnackResponse::default())
                     }
-                })
-            }))
-        });
-        // `serve` を使ってサーバーを起動
-        aqua::serve(listener, make_service, make_connect_service)
-            .await
-            .unwrap();
+                }
+            })
+        }))
     });
-
+    let listener = TcpListener::bind(addr).await?;
+    aqua::serve(listener, make_service, make_connect_service).await?;
     Ok(())
 }
 
@@ -118,14 +120,14 @@ pub mod topic_manager {
             Self { topic_map }
         }
 
-        pub fn register(&mut self, topic_filter: String, client_id: Uuid, option: SubscribeOption) {
+        pub fn register(&self, topic_filter: String, client_id: Uuid, option: &SubscribeOption) {
             self.topic_map
                 .entry(topic_filter)
                 .or_default()
-                .push((client_id, option));
+                .push((client_id, option.clone()));
         }
 
-        pub fn unregister(&mut self, topic_filter: String, client_id: Uuid) {
+        pub fn unregister(&self, topic_filter: String, client_id: Uuid) {
             if let Some(mut v) = self.topic_map.get_mut(&topic_filter) {
                 v.retain(|x| x.0 != client_id);
             }
